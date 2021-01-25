f615415897869250f9654adcc4cf5d816b5ef357
Reinholds Zviedris <reinholds@zviedris.lv>
2021-01-26 00:40:50 +0200

    bump gcloud module version

--- cluster.tf
+++ cluster.tf
@@ -322,7 +322,7 @@ resource "google_container_node_pool" "pools" {
 
 module "gcloud_wait_for_cluster" {
   source  = "terraform-google-modules/gcloud/google"
-  version = "~> 2.0.2"
+  version = "~> 2.0.3"
   enabled = ! var.skip_provisioners
   upgrade = var.gcloud_upgrade
 
